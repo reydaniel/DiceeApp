665fce087614d8b7a7305e66e84ec463236ed652
reydaniel <reynaldidaniel@outlook.com>
2021-03-26 15:37:55 +0700

    Create some component in interface builder to show dice. When the button clicked, it will change the dice on the left to be increment by 1 and the dice on the right will be decrement by 1.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    var arrayDice = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButton(_ sender: UIButton) {
+        diceImageViewOne.image = arrayDice[leftDiceNumber]
+        diceImageViewTwo.image = arrayDice[rightDiceNumber]
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
+    
 }
 
